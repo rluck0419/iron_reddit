9d481d3b9baf4d0bd8284c709c3b9ab00ed0d284
Robert Luckfield <rluck0419@gmail.com>
2016-07-27 14:14:25 -0500

    basic functionality for index/show link -- working on pagination and submission pages

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ApplicationRecord
   has_many :votes
   validates :title, presence: true
   validates :url, presence: true
+  max_paginates_per 20
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+40.times do |i|
+  link = Link.create!(title: Faker::Name.name, url: "www.#{Faker::Name.name.downcase.gsub(/[. ']/, '')}.com")
+  rand(1..5).times do
+    Vote.create!(link: link)
+  end
+end
