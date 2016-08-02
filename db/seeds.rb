256f0f5524d617e8e2d35a43826cbf17d49e45d8
Robert Luckfield <rluck0419@gmail.com>
2016-08-01 22:47:16 -0500

    push with new faker-esque data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+BOARDS = ["art", "movies", "music", "computers", "games", "omg", "wow", "yolo", "thuglife", "funny"]
+BAIT = ["You'll Never Guess Which", "The Top 10", "Click Here to See", "You'd Never Believe"]
+SUBJ = ["Celebs", "Billionares", "Illuminati", "Entrepreneurs", "New Age Retro Hippies", "Cranky Ladies"]
+ACT = ["Spy", "Drink", "Eat", "Smoke", "Gamble"]
+PRED = ["On Each Other", "Way Too Much", "With Death", "Behind Each Other's Back"]
+
+URLS = ["clickybait", "listicles-please", "timewaster", "scuttlebutt-daily", "grapevine-report", "scandalist"]
+
 10.times do |i|
-  board = Board.create!(name: Faker::Hacker.noun)
+  board = Board.create!(name: BOARDS[i])
   user = User.create!(email: "user#{i+1}@example.com", password: "password")
 
-  10.times do
-    link = Link.create!(title: Faker::Name.name, url: "www.#{Faker::Name.name.downcase.gsub(/[. ']/, '')}.com", user: user, board: board)
+  20.times do
+    link = Link.create!(title: "#{BAIT.sample} #{SUBJ.sample} #{ACT.sample} #{PRED.sample}", url: "www.#{URLS.sample}.com/trash/#{rand(500).round}", user: user, board: board)
 
-    10.times do
+    50.times do
       if rand > 0.5
         Upvote.create!(user_id: user.id, link_id: link.id)
       else
